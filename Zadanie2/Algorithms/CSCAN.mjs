b99fe93bd723f0752243c9e663bc9634245d3912
Jakub Radzik <42437524+radzikoska123@users.noreply.github.com>
2021-04-14 17:44:31 +0200

    Add files via upload

--- Zadanie2/Algorithms/CSCAN.mjs
+++ Zadanie2/Algorithms/CSCAN.mjs
@@ -3,24 +3,25 @@ export const CSCAN = (tab, start, end, currentPosition) => {
     let time = 0;
     let newTab = tab.slice(0); //slice of array works as a copy
 
-    while(true){
+    while(newTab.length>0){
         for (let j = 0; j < newTab.length; j++) {
-            if(newTab[j].cylinder===currentPosition){
+            if(newTab[j].cylinder===currentPosition && newTab[j].enterMoment<=time){
                 time+=newTab[j].duration;
                 newTab.splice(j,1);
+            }
 
-                if(newTab.length===0){
-                    return sumOfMoves;
-                }
+            if(newTab.length===0){
+                return sumOfMoves;
             }
         }
 
         if(currentPosition===end){
-            currentPosition=start+1;
+            currentPosition=start-1;
         }
 
         currentPosition++;
         sumOfMoves++;
+        time++;
     }
 
 
